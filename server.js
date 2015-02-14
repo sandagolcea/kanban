b87890d68d9761d183dbb3c999b1922a2b6adbd7
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-14 19:08:59 +0000

    Basic post, get and redirect for one card

--- server.js
+++ server.js
@@ -3,12 +3,24 @@ var server = http.createServer();
 var express = require('express');
 var app = express();
 
+var bodyParser = require('body-parser');
+var parseUrlencoded = bodyParser.urlencoded({extended: false});
+
+var cardList = {};
+
 app.set('view engine','ejs');
 app.use(express.static(__dirname+'/public'));
 
-app.get('/',function(request,response){
-  response.render('index');
+app.get('/', function(request,response){
+  response.render('index',{list : cardList});
 });
 
-app.listen(8080);
-console.log("Application listening on localhost:8080..");
+app.post('/cards', parseUrlencoded, function (request, response) {
+  cardList = request.body.card;
+  console.log(cardList);
+  response.redirect('/');
+});
+
+app.listen(8080, function(){
+  console.log("Application listening on localhost:8080..");
+});
