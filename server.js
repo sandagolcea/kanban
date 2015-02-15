af958e8866b5ff0e0cba83a02f40c6390746341b
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-15 01:29:45 +0000

    Can now add lists with cards on them

--- server.js
+++ server.js
@@ -6,7 +6,9 @@ var app = express();
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({extended: false});
 
-var mainList = [];
+var mainList = {};
+var listId = 0;
+var cardId = 0;
 
 app.set('view engine','ejs');
 app.use(express.static(__dirname+'/public'));
@@ -16,16 +18,40 @@ app.get('/', function (request,response) {
 });
 
 app.post('/lists', parseUrlencoded, function (request, response) {
-  mainList.push(request.body.list);
+  mainList[listId] = {name: request.body.list, cards: []};
+  console.log(mainList[listId]);
+  listId++;
   response.redirect('/');
 });
 
 app.post('/cards', parseUrlencoded, function (request,response) {
-  console.log(request.body.list);
-  console.log(request.body.card);
+  list = request.body.listId;
+  if (!mainList[list]) console.log("mainList null");
+  console.log("Card:"+request.body.card);
+  mainList[list].cards.push({id: cardId, content: request.body.card});
+  console.log("Main cards:"+mainList[list].cards);
   response.redirect('/');
 });
 
 app.listen(8080, function(){
   console.log("Application listening on localhost:8080..");
 });
+/*
+
+This is how my mainList object looks like
+
+{ "1234" : {
+  name: "Sanda",
+  cards: [ 
+          { id: 1, content: "My first card"},
+          { id: 2, content: "My second card"}
+  ]}, 
+  "432" : {
+  name: "Jair",
+  cards: [ 
+          { id: 1, content: "My first card"},
+          { id: 2, content: "My second card"}
+  ]} 
+}
+
+*/
