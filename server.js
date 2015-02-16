8faf83af2d6f305eb87dbc6e31b0054d349796ac
Sanda Golcea <sanda.golcea@gmail.com>
2015-02-16 21:35:33 +0000

    Refactor and rename, like: a lot! crazy loco

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var server = http.createServer();
 var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
-require('./lists.js');
+require('./models.js');
 
 var bodyParser = require('body-parser');
 var parseUrlencoded = bodyParser.urlencoded({extended: false});
@@ -16,68 +16,73 @@ app.set('view engine','ejs');
 app.use(express.static(__dirname+'/public'));
 
 app.get('/', function(request,response){
-  response.redirect('/boards');
+  response.redirect('/dashboard');
 });
 
-app.get('/boards', function(request,response){
+// shows a dashboard (collection of boards)
+app.get('/dashboard', function(request,response){
   Board.find()
   .populate('lists')
   .exec(function (err, myBoard) {
     if (err) return console.error(err);
-    response.render('boards', {board: myBoard});
+    response.render('dashboard', {board: myBoard});
   });
 });
 
-app.get('/lists/:id', function (request,response) {
+// shows a board (collection of lists)
+app.get('/board/:id', function (request,response) {
   console.log(request.params.id);
   List.find({_creator : request.params.id })
   .populate('cards') // only works if we pushed refs to children
   .exec(function (err, myLists) {
     if (err) return console.error(err);
-    response.render('index',{boardId: request.params.id, list : myLists});
+    response.render('board',{boardId: request.params.id, list : myLists});
   });
 });
 
-app.post('/boards', parseUrlencoded, function (request, response) {
+// creates a new board and redirects back to dashboard
+app.post('/board', parseUrlencoded, function (request, response) {
   console.log("Body is:"+request.body.boardId);
   new Board({ name: request.body.board })
   .save(function (err, newBoard) {
     if (err) return console.error(err);
-    response.redirect('/boards');  
+    response.redirect('/dashboard');  
   });
 });
 
-app.post('/lists/:id', parseUrlencoded, function (request, response) {
+// creates a new list and redirects back to board
+app.post('/list/:boardId', parseUrlencoded, function (request, response) {
   new List({ 
-    _creator: request.params.id,
+    _creator: request.params.boardId,
     name: request.body.list })
     .save(function (err, newList) {
         if (err) return console.error(err);
-        Board.findOne({_id: request.params.id}, function (err, board) {
+        Board.findOne({_id: request.params.boardId}, function (err, board) {
           if (err) return console.error(err);
           board.lists.push(newList);
           board.save(function (err) {
             if (err) return handleError(err);
-            response.redirect('/lists/' + request.params.id);
+            response.redirect('/board/' + request.params.boardId);
           });
         })
     });
 });
 
-app.post('/cards', parseUrlencoded, function (request,response) {
+// creates a new card and redirects back to board
+app.post('/card/:listId/:boardId', parseUrlencoded, function (request,response) {
   new Card({ 
-    _creator: request.body.listId, 
+    _creator: request.params.listId, 
     content: request.body.card })
     .save(function (err, newCard) {
       if (err) return console.error(err);
 
-      List.findOne({ _id: request.body.listId }, function (err, list) {
+      List.findOne({ _id: request.params.listId }, function (err, list) {
         if (err) return console.error(err);
         list.cards.push(newCard);
 
         list.save(function (err) {
           if (err) return handleError(err);
-          response.redirect('/lists/'+request.body.creatorBoardId);
+          response.redirect('/board/'+request.params.boardId);
         });
       });
   });
